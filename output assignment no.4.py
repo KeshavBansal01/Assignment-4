18744ee5d8074a5ef87af147232a7b5de1ce5eda
KeshavBansal01 <97675847+KeshavBansal01@users.noreply.github.com>
2022-02-28 19:52:20 +0530

    Add files via upload

---
+++ output assignment no.4.py
@@ -0,0 +1,93 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/Keshav Bansal/AppData/Local/Programs/Python/Python310/Assignment no.4.py
+Question 1
+Enter the  number of disks in the  tower of Hanoi:4
+
+These  disks are numbered starting from  the top of  tower.
+ Some steps to move the disks from Source Tower to a Destination Tower is given below:
+
+Move disk 1 from Source Tower to Auxiliary
+Move disk 2 from Source Tower to Destination
+Move disk 1 from Auxiliary to Destination
+Move disk 3 from Source Tower to Auxiliary
+Move disk 1 from Destination to Source Tower
+Move disk 2 from Destination to Auxiliary
+Move disk 1 from Source Tower to Auxiliary
+Move disk 4 from Source Tower to Destination
+Move disk 1 from Auxiliary to Destination
+Move disk 2 from Auxiliary to Source Tower
+Move disk 1 from Destination to Source Tower
+Move disk 3 from Auxiliary to Destination
+Move disk 1 from Source Tower to Auxiliary
+Move disk 2 from Source Tower to Destination
+Move disk 1 from Auxiliary to Destination
+
+Question 2
+
+Enter the number of rows for Pascal's Triangle: 5
+Using recursions to create pascal's triangle: 
+        1   
+      1   1   
+    1   2   1   
+  1   3   3   1   
+1   4   6   4   1   
+Using loops to create pascal's triangle:
+        1   
+Question #3
+      1   1   
+Question #3
+    1   2   1   
+Question #3
+  1   3   3   1   
+Question #3
+1   4   6   4   1   
+Question #3
+ 
+ 
+Enter your first integer: 6
+Enter yourfirst integer: 7
+Remainder is:  6
+Quotient is:  0
+One of  the value is zero
+{10}
+{10, 11}
+{10, 11, 5}
+{10, 11, 12, 5}
+{5, 6, 10, 11, 12}
+{5, 6, 10, 11, 12}
+Immutable set:  frozenset({5, 6, 10, 11, 12})
+Largest value in the set:  12
+Hash value:  12
+
+
+[Question.4]
+
+Hello, Myself   Keshav and my roll no. is 21105096
+Destructor Called
+
+[Question.5]
+
+Employee Name is Mehak and Salary is 40000 
+Employee Name is Ashok and Salary is 50000 
+Employee Name is Viren and Salary is 60000 
+
+Que.5a
+Mehak salary Updated to 70000
+Employee Name is Mehak and Salary is 70000 
+
+Que.5b
+Employee Viren's data has been removed.
+ 
+Enter  first word: keshav
+
+You are supposed to enter a new meaningful word to test your friendship: aman
+These letters aren't exact,  your friendship is fake!!
+
+Does the word makes sense?(y or n)
+n
+This word doesn't make sense,your friendship is fake!!
+
+
+
